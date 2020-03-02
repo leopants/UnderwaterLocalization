b4d6c88ae7ac34ef6005aebc3e549e44973522ba
Leonardo Pantaleon <lpant011@fiu.edu>
2020-03-01 21:39:46 -0500

    first commit

--- main.py
+++ main.py
@@ -3,5 +3,7 @@ import json
 
 data = pd.read_csv("june11_10_30.log", ';')
 
-print(data[['Latitude', 'Longitude', 'Time', 'Date',
-            'Total Water Column (m)', 'Temperature (c)', 'pH', 'ODO mg/L']])
+selected_data = data[['Latitude', 'Longitude', 'Time', 'Date',
+                      'Total Water Column (m)', 'Temperature (c)', 'pH', 'ODO mg/L']]
+
+print(selected_data)
