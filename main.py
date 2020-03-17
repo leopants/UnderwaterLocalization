0fe2c2eea2bb3ecc20b050e2eda8627e42a6c4ca
Leonardo Pantaleon <lpant011@fiu.edu>
2020-03-17 12:21:14 -0400

    removed outliers

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 import pandas as pd
+import numpy as np
+from scipy import stats
 
+# Read the selected file and create a pandas DataFrame
 data = pd.read_csv("june11_10_30.log", ';')
 
+# Select the necessary tags from the entire data set as a seperate DataFrame
 selected_data = data[['Latitude', 'Longitude', 'Time', 'Date',
                       'Total Water Column (m)', 'Temperature (c)', 'pH', 'ODO mg/L']]
 
-print(selected_data)
+# Z-Score cannot be used with strings so remove string from the possible tags
+outlier_possible = selected_data[['Latitude', 'Longitude', 'Total Water Column (m)',
+                                  'Temperature (c)', 'pH', 'ODO mg/L']]
+
+abs_z = np.abs(stats.zscore(outlier_possible))
+filter_z = (abs_z < 3).all(axis=1)
+print(filter_z)
+outlier_possible = outlier_possible[filter_z]
+
+print(outlier_possible)
